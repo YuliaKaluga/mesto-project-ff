c8451a137b85d07516c4c541bbab4412979e7fd6
Yulia Kaluga <yuliakaluga0792@yandex.ru>
2024-09-25 11:57:35 +0400

    сдача ПР5 25-08(1)

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,31 @@
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
+function createCard(card, deleteCard) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.cloneNode(true);
+
+  cardElement.querySelector(".card__image").src = card.link;
+  cardElement.querySelector(".card__image").alt = card.name;
+  cardElement.querySelector(".card__title").textContent = card.name;
+
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", function () {
+    deleteCard(cardElement);
+  });
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard() {
+  const cardElement = document.querySelector(".card");
+  cardElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+const cardsContainer = document.querySelector(".places__list");
+initialCards.forEach(function (card) {
+  const cardElement = createCard(card, deleteCard);
+  cardsContainer.append(cardElement);
+});
